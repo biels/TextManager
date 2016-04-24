54f7ea5759b56b9ba6beb601ac36d16573de85fd
Biel <Biel@192.168.1.34>
2016-04-24 10:47:51 +0200

    Added sample unit test

---
+++ TextManager/test/test1.cpp
@@ -0,0 +1,16 @@
+/*
+ * test1.cpp
+ *
+ *  Created on: Apr 24, 2016
+ *      Author: Biel
+ */
+#include "gtest/gtest.h"
+#include "../src/Logic/Entities/Text.h"
+#include <string>
+TEST(TextClass, TitleIntegrity){
+	Text t;
+	//const char* testTitle = "TestTitle";
+	//t.setTitle(testTitle);
+	//EXPECT_TRUE(t.getTitle() == testTitle);
+}
+
