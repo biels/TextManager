7f183401fda91b3e5d512f9f57c735b2495ff7e6
Biel <Biel@10.192.86.229>
2016-04-29 11:28:03 +0200

    Defined QuoteSet's public operations

--- TextManager/src/Logic/Data/QuoteSet.h
+++ TextManager/src/Logic/Data/QuoteSet.h
@@ -9,9 +9,21 @@
 #define LOGIC_DATA_QUOTESET_H_
 
 class QuoteSet {
+private:
+
 public:
 	QuoteSet();
 	virtual ~QuoteSet();
+
+	void add(Quote q);
+	void remove(Quote q);
+	void remove(int id);
+	bool exists(Quote q);
+	bool exists(int id);
+	Quote getByID(int id);
+	QuoteSet getAllByID(int id);
+	QuoteSet getAll();
+
 };
 
 #endif /* LOGIC_DATA_QUOTESET_H_ */
