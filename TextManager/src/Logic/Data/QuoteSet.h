0f76fe8d64508dca6d0c213b6faf9e01348f4acc
Biel <Biel@BIEL-TOSHIBA>
2016-04-29 20:49:21 +0200

    Documented and implemented QuoteSet

--- TextManager/src/Logic/Data/QuoteSet.h
+++ TextManager/src/Logic/Data/QuoteSet.h
@@ -34,18 +34,31 @@ public:
 	 * \pre Cert
 	 * \post Elimina la cita amb identificador id del paràmetre implícit
 	 */
-	void removeByID(int id);
+	void remove(int id);
 
 	/**@brief Comprova si existeix una cita
 	 * \pre Cert
 	 * \post
 	 */
-	bool exists(Quote q);
-	bool existsWithID(int id);
-	Quote getByID(int id);
-	QuoteSet getAllByID(int id);
-	QuoteSet getAll();
+	bool exists(int id);
 
+	/**@brief Consulta la cita amb identificador id
+	 * \pre Existeix una cita am identificador id
+	 * \post El resultat és la cita amb identificador id
+	 */
+	Quote getByID(int id);
+
+	/**@brief Consulta totes les cites d'un autor amb identificador id
+	 * \pre Existeix un autor amb identificador id
+	 * \post El resultat és un conjunt de cites amb totes les cites de l'autor amb identificador id
+	 */
+	QuoteSet getAllByAuthor(int id);
+
+	/**@brief Consulta totes les cites que apareixen en un text amb identificador id
+	 * \pre Existeix un text amb identificador id
+	 * \post El resultat és un conjunt de cites que conté totes les cites que apareixen al text amb identificador id
+	 */
+	QuoteSet getAllByText(int id);
 };
 
 #endif /* LOGIC_DATA_QUOTESET_H_ */
--- TextManager/src/Logic/Data/TextSet.h
+++ TextManager/src/Logic/Data/TextSet.h
@@ -15,12 +15,53 @@ using namespace std;
 
 class TextSet {
 private:
-
+	int nextID;
 public:
 	TextSet();
 	virtual ~TextSet();
-	Text getTextByTitle(string title);
-	TextSet getTextsByAuthor(string author);
+
+	/**@brief Calcula l'identificador del següent elememnt del conjunt
+	 * \pre Cert
+	 * \post El resultat és l'identificador que ha de tenir el següent element del conjunt
+	 */
+	int getNextID();
+
+	/**@brief Afegeix un text al conjunt
+	 * \pre Cert
+	 * \post El paràmetre implícit conté el text t
+	 */
+	void add(Text t);
+
+	/**@brief Elimina un text amb identificador id
+	 * \pre Cert
+	 * \post Elimina el text amb identificador id si existeix
+	 */
+	void remove(int id);
+
+	/**@brief Elimina una cita
+	 * \pre Cert
+	 * \post Elimina la cita q del paràmetre implícit
+	 */
+	void remove(const Quote& q);
+
+	/**@brief Comprova si l'element amb identificador id pertany al conjunt
+	 * \pre Cert
+	 * \post El resultat és cert sí i només sí el text amb identificador id existeix
+	 */
+	bool exists(int id);
+
+	Text get(int id);
+	/**@brief Obté un text amb el títol especificat
+	 * \pre Existeix untext amb títol t
+	 * \post El resultat és el text amb el títol especificat
+	 */
+	Text getByTitle(string t);
+
+	/**@brief Obté tots els textos de l'autor amb identificador authorID
+	 * \pre Existeix un autor amb el títol authorID
+	 * \post El resultat és un conjunt de textos que conté els textos de l'autor id
+	 */
+	TextSet getAllByAuthor(int id);
 };
 
 #endif /* LOGIC_DATA_TEXTSET_H_ */
