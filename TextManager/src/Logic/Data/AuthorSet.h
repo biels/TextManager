d444f2e782c8aa6a634e82f27dc6a5755cd126c3
Biel <Biel@10.192.86.229>
2016-04-29 12:02:45 +0200

    Defined AuthorSet

--- TextManager/src/Logic/Data/AuthorSet.h
+++ TextManager/src/Logic/Data/AuthorSet.h
@@ -7,11 +7,16 @@
 
 #ifndef LOGIC_DATA_AUTHORSET_H_
 #define LOGIC_DATA_AUTHORSET_H_
-
 class AuthorSet {
 public:
 	AuthorSet();
 	virtual ~AuthorSet();
+	//TODO Getters and setters
+	void add(Author a);
+	void remove(Author a);
+	void remove(int id);
+	Author getByID(int authorID);
+
 };
 
 #endif /* LOGIC_DATA_AUTHORSET_H_ */
