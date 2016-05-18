178077dc5281eed4738e1a3086bc0bb33a8f23c2
Biel Simon Rojas <bielsimon2@gmail.com>
2016-05-18 22:04:57 +0200

    Continued documenting AH

--- TextManager/src/Logic/Actions/ActionHandler.h
+++ TextManager/src/Logic/Actions/ActionHandler.h
@@ -60,12 +60,37 @@ public:
 	void textosAutor (string autor);
 	void textos();
 	void autors();
+	/**
+	 * @brief Mostra informació de l'últim text triat
+	 * (autor, títol, nombre de frases, nombre de paraules i cites associades)
+	 */
 	void info();
-	string autor();
+	/**
+	 * @brief Mostra l'autor de l'últim text triat.
+	 */
+	void autor();
+	/**@brief mostra el contingut de l'ultim text triat
+	 * Amb les seves frases numerades a partir de l'1.
+	 * Per mostrar una frase, s'escriura el numero de frase seguit d'un espai i de la frase, amb un unic salt de lnia al nal d'aquesta.
+	 * En el format de sortida, els signes de puntuacio aniran enganxats a la paraula previa i no hi haura ni tabuladors ni dos espais seguits.
+	 */
 	void contingut();
+	/**@brief Mostra les frases entre la x-esima i la y-esima del contingut de l'ultim text triat
+	 * extrems inclosos (si x = y, no es repeteix la frase). Aquesta comanda requereix un interval valid de frases, es a dir, que 1  x  y  n, on n es el nombre total de frases.
+	 * @param x x-èssima frase
+	 * @param y y-èssima frase
+	 */
 	void frases(int x, int y);
-	int nombreFrases();
-	int nombreParaules();
+	/**@brief Mostra el nombre de frases del contingut de l'ultim text triat.
+	 */
+	void nombreFrases();
+	/**@brief Mostra el nombre de paraules del contingut de l'ultim text triat.
+	 */
+	void nombreParaules();
+	/**@brief mostra totes les paraules del contingut del text ordenades decreixentment per frequencia
+	 * (per a cada paraula la cadena que la representa i la seva frequencia) de l'ultim text triat.
+	 * En cas d'empat de frequencia les paraules s'ordenen creixement, primer per llargada i despres alfabeticament.
+	 */
 	void taulaFrequencies();
 	void frasesExpressio(string exp);
 	void frasesSequencia();
