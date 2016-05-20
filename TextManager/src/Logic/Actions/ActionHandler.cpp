18beca2aea954df92aee9660b8c3c2968e0f725d
Biel Simon Rojas <bielsimon2@gmail.com>
2016-05-20 12:01:47 +0200

    Finished ActionHandler implementation

--- TextManager/src/Logic/Actions/ActionHandler.cpp
+++ TextManager/src/Logic/Actions/ActionHandler.cpp
@@ -46,14 +46,48 @@ void ActionHandler::substitueix(string match, string replace){
 	cout << "Replaced";
 }
 void ActionHandler::textosAutor (string autor){
-	//c.getTs().printAllByAuthor(c.getAs().findByName(autor), c);
+	c.getTs().printAllByAuthor(c.getAs().findByName(autor), c);
 }
 void ActionHandler::textos(){
-	//c.getTs().printAll(c);
+	c.getTs().printAll(c);
 }
 void ActionHandler::autors(){
-	//c.getAs().printAuthorList();
+	c.getAs().printAuthorList();
 }
 void ActionHandler::info(){
-	c.getTs().get(c.getChosenTextId()).printInfo(c);
+	c.getChosenText().printInfo(c);
+}
+void ActionHandler::autor(){
+	c.getChosenText().getAuthor(c).print();
+}
+void ActionHandler::contingut(){
+	c.getChosenText().printContent();
+}
+void ActionHandler::frases(int x, int y){
+	c.getChosenText().printSentenceListInRange(x, y);
+}
+void ActionHandler::nombreFrases(){
+	cout << c.getChosenText().getSentenceCount();
+}
+void ActionHandler::nombreParaules(){
+	cout << c.getChosenText().getWordCount();
+}
+void ActionHandler::taulaFrequencies(){
+	c.getChosenText().printFrequencyTable();
+}
+void ActionHandler::frasesSequencia(string seq){
+	c.getChosenText().printSentenceListContaining(seq);
+}
+void ActionHandler::frasesExpressio(string exp){
+	c.getChosenText().printSentenceListMatchingExpression(exp);
+}
+//Quotes
+void ActionHandler::cites(){
+	c.getQs().printAll(c);
+}
+void ActionHandler::citesAutor(string nom){
+	c.getQs().printAllByAuthor(c.getAs().findByName(nom), c);
+}
+void ActionHandler::eliminarCita(string ref){
+	c.getQs().remove(c.getQs().findByRef(ref));
 }
