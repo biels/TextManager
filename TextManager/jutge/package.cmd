d91a673673cf4a4e3fb9dc8879d190338efe346f
Biel Simon Rojas <bielsimon2@gmail.com>
2016-05-29 12:13:34 +0200

    Fixed makefile and packaging script

--- TextManager/jutge/package.cmd
+++ TextManager/jutge/package.cmd
@@ -3,8 +3,8 @@ mkdir package
 for /R ../src %%f in (*.h) do copy %%f package\
 for /R ../src %%f in (*.cpp) do copy %%f package\
 for /R package %%f in (*.*) do sed -i -b -r 's/#include ".+\//#include "/g' %%f
-cp makefile.txt package/makefile
+cp makefile.txt package/Makefile
 cd package\
 ren *.cpp *.cc
 ren TextManager.cc program.cc
-tar -cvf ../practica.tar *.*
+tar -cvf ../practica.tar Makefile *.*
