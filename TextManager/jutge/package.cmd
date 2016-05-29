a28c5a5a7cc7a59cefaacb526d220335e08abd5c
Biel Simon Rojas <bielsimon2@gmail.com>
2016-05-29 04:14:05 +0200

    Created export to jutge system

---
+++ TextManager/jutge/package.cmd
@@ -0,0 +1,10 @@
+rd /S /Q package
+mkdir package
+for /R ../src %%f in (*.h) do copy %%f package\
+for /R ../src %%f in (*.cpp) do copy %%f package\
+for /R package %%f in (*.*) do sed -i -b -r 's/#include ".+\//#include "/g' %%f
+cp makefile.txt package/makefile
+cd package\
+ren *.cpp *.cc
+ren TextManager.cc program.cc
+tar -cvf ../practica.tar *.*
