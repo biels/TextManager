d7437a1654c402aff1af15a70b6d8bba82249252
Alba <alba.canete97@gmail.com>
2016-05-31 10:33:53 +0200

    doxygen done

--- TextManager/src/Logic/Entities/Author.h
+++ TextManager/src/Logic/Entities/Author.h
@@ -12,14 +12,17 @@
 #include <string>
 
 using namespace std;
+
 /** @class Author
  *  @brief Representa un autor, una seqüència de paraules que conté el nom i el cognom d'una sola persona.
  */
+
 class Author {
 private:
 	int id;
 	string name;
 	string initials;
+
 public:
 	/** @brief Crea un autor amb l'identificador especificat.
 	 * \pre Cert.
@@ -44,9 +47,9 @@ public:
 	/** @brief Estableix el nom de l'autor.
 	 *  \pre Cert.
 	 *  \post name és el nom del paràmetre implícit.
+	 *  @param name nom de l'autor
 	 */
 	void setName(const string name);
-
 
 	/** @brief Mostra les inicials de l'autor
 	 * \pre S'han utilitzat amb èxit les accions setName() i setLastName() del paràmetre implícit. @see setName() @see setLastName()
