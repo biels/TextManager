7da3c874109fd167e446ea8d580fb13e37626a41
Biel Simon Rojas <bielsimon2@gmail.com>
2016-05-31 23:15:47 +0200

    Documented CE

--- TextManager/src/UI/CommandExecutor.h
+++ TextManager/src/UI/CommandExecutor.h
@@ -11,19 +11,29 @@ using namespace std;
 #include <vector>
 #include <string>
 #include "../Logic/Actions/ActionHandler.h"
-#include "../Logic/Actions/DataStructures/ConditionalExpression.h"
 
 /**@class CommandExecutor
  * @brief Interpreta les comandes que entren pel canal d'entrada estàndar.
- * Aquesta classe s'encarrega únicament de processar la entrada i cridar les accions corresponents a l'ActionHandler.
+ * Aquesta classe s'encarrega únicament de processar l'entrada i cridar les accions corresponents a l'ActionHandler.
  */
 
 class CommandExecutor {
 private:
 	ActionHandler a;
+	/** @brief Executa la comanda especificada
+	 * \pre args és una comanda amb format vàlid
+	 * \post La comanda s'ha executat
+	 */
 	void executeCommand(string args);
+
+	/**@brief Executa una comanda a partir de les paraules clau i els arguments.
+	 * \pre Les paraules clau són reconegudes per l'especificació del sistema i els arguments es troben entre delimitadors.
+	 * \post La comanda s'ha executat
+	 * @param keywords Paraules clau de la comanda
+	 * @param question Cert si la comanda acaba amb ?
+	 * @param args_s Arguments de la comanda amb delimitadors
+	 */
 	void executeCommand(vector<string> keywords, bool question, vector<string> args_s);
-	static ConditionalExpression parseConditionalExpression(string expr);
 
 public:
 	/**
@@ -38,7 +48,7 @@ public:
 	 */
 	virtual ~CommandExecutor();
 
-	/**@brief Llegeix el canal d'entrada estàndar.
+	/**@brief Llegeix el canal d'entrada estàndard i executa les comandes que hi entrin.
 	 * \pre Cert
 	 * \post El programa finalitzarà la seva execució, perquè l'usuari ho ha demanat.
 	 */
